bd228e4fe49f15fdcb430ec35a2d7b759396fd5d
tailypoe3 <tailypoe3@gmail.com>
2022-12-09 11:27:42 -0500

    Edit getComputerChoice function
    Function getComputerChoice edited and changed from array to switch
    function. While the array option looks cleaner, this is in order to
    avoid possible future errors until I learn more about arrays.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,6 +1,30 @@
 function getComputerChoice() {
-  let choices = ['rock', 'paper', 'scissors']
-  return choices[Math.floor(Math.random() * choices.length)]
-}
+  let randomChoice = Math.floor(Math.random() * 3);
+    switch(randomChoice) {
+      case 0:
+        return 'rock';
+        break;
+      case 1: 
+        return 'paper';
+        break;
+      case 2:
+        return 'scissors';
+        break;
+    }
+  }
 console.log(getComputerChoice())
 
+
+
+// function playRound(playerSelection, computerSelection) {
+  
+// }
+
+// const playerSelection = 'Rock';
+// const computerSelection = getComputerChoice();
+// console.log(playRound(playerSelection, computerSelection));
+
+// function game() {
+//   for(let i = 0; i < 5; i++)
+//   //code
+// }
